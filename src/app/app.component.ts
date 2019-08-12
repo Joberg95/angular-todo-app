8171ab4f0a5b950a23fe821b27c4bd42ce12f808
nevroje <joakim.berglind@gmail.com>
2019-08-12 14:06:34 +0200

    created delet method in ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  todoArray=[];
-  addTodo(value){
-    this.todoArray.push(value)
-    console.log(this.todoArray)
+  todoArray = [];
+  todo;
+
+  addTodo(value) {
+    this.todoArray.push(value);
+    //console.log(this.todoArray)
+  }
+
+  deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1);
+      }
+    }
   }
 }
